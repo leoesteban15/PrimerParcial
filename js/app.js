78a7ae50c8e96e68cc7bd213bde94c01bff2e5a0
Leonardo <248530544+leoesteban15@users.noreply.github.com>
2026-09-25 07:58:34 -0500

    Mejorar estructura y codigo para fetching de la ropa

--- js/app.js
+++ js/app.js
@@ -14,21 +14,36 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     var loginForm = document.getElementById("loginForm");
-//     if(loginForm){
-//         console.log("2");
-//         loginForm.addEventListener("submit", function(event) {
-//             event.preventDefault();
+async function getData() {
+  try {
+    const response = await fetch(
+      "https://fakestoreapi.com/products"
+    );
 
-//             var username = document.getElementById("username").value;
-//             var password = document.getElementById("password").value;
-//             console.log(2);
+    if (!response.ok) {
+      throw new Error(`Estado de la respuesta: ${response.status}`);
+    }
 
-//             if(username && password){
-//                 window.location.href = "./tienda.html";
-//                 console.log(2);
-//             }
-//         });
-//     }
-// });
+    const result = await response.json();
+    console.log(result);
+
+    const container = document.querySelector("#containter-clothes");
+
+    result.forEach(clothe => {
+      const clotheCard = document.createElement("div");
+      clotheCard.classList.add("clotheCard");
+
+      clotheCard.innerHTML = `
+        <h3>${clothe.title}</h3>
+        <img src="${clothe.image}" alt="${clothe.title}">
+      `;
+
+      container.appendChild(clotheCard);
+    });
+
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData();
